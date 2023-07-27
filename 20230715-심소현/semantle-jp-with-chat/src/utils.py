d1d79a63906da2bc31fad70b5d3bffbfeebcc422
Sohyun Sim <sohyun.sim27@gmail.com>
2023-07-27 10:43:31 +0900

    added submodule

--- 20230715-심소현/semantle-jp-with-chat/src/utils.py
+++
@@ -1,6 +0,0 @@
-def format_result(word, sim, rank):
-    return f"{word}: スコア {sim}, ランク {rank}"
-
-def format_table(table, n_rows=10):
-    top_results = table.sort_values(by="スコア", ascending=False).head(n_rows)
-    return top_results.to_markdown(index=False)
